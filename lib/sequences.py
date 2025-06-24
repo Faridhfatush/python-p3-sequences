6a302e35d99f77e015a14e11bbd632cdc3159c39
Fatuma Dahir <faridhfatush2024@gmail.com>
2025-06-24 12:01:26 +0300

    lab done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+      if (length == 0) :
+        print ([])
+      elif (length == 1) :
+        print ([0])
+      elif (length == 2) :
+         print( [0, 1] )
+      else:
+           result = [0, 1]
+           while len(result) < length:
+            result.append(result[-1] + result[-2])
+           print(result) 
+print(print_fibonacci(10))
+
